22e313b7916af83bf997693d9542b4c54df6fd13
jelenastj <jelena.stjepanovic@gmail.com>
2020-09-27 15:59:36 -0700

    notes1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,61 @@
 const validator = require('validator')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const message = getNotes()
-console.log(message);
+//Customize yargs version
+yargs.version('1.0.0')
 
-console.log(validator.isEmail('g.com'));
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demaindOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demaindOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Body:', argv.body);
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing note')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function(){
+        console.log('Listing notes')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+//add, remove, read, list
+
+
+yargs.parse()
+//console.log(yargs.argv);
+
+
